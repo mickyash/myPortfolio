7401ebbd26d3ac86474ce5138a6a471d73f50c54
Yash <sharmayash2862@gmail.com>
2024-01-07 01:06:20 +0530

    changed again

--- server.cjs
+++ server.cjs
@@ -7,9 +7,9 @@ const path = require('path')
 const app = express();
 
 
-app.use(express.static(path.join(__dirname,'./')))
+app.use(express.static(path.join(__dirname,'myportfolio')))
 app.get('*',function(req,res){
-  res.sendFile(path.join(__dirname, './index.html'))
+  res.sendFile(path.join(__dirname, 'myportfolio','ndex.html'))
 })
 
 app.use(cors());
